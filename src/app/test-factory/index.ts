9f6d1756f3715c5223360cee4acbc174cc04d734
RipperZero <1018744707@qq.com>
2022-09-26 17:17:08 +0900

    feat: add test electron IpcRenderer

--- src/app/test-factory/index.ts
+++ src/app/test-factory/index.ts
@@ -1,4 +1,5 @@
 // export { DragHandle as TestComponent } from "./components/dnd/DragHandle";
 // export { Position as TestComponent } from "./components/Position";
 // export { Immer as TestComponent } from "./components/immer/Immer";
-export { TestContext as TestComponent } from "./components/context/TestContext";
+// export { TestContext as TestComponent } from "./components/context/TestContext";
+export { IpcRenderer as TestComponent } from "./components/ipcRenderer/IpcRenderer";
