ba41dc370358432388527a163a8ffc99cd9f1381
RipperZero <1018744707@qq.com>
2022-10-03 11:51:38 +0800

    feat: update analyse lodash

--- src/app/test-factory/no-render/lodash/_temp/test.js
+++ src/app/test-factory/no-render/lodash/_temp/test.js
@@ -1,6 +1,13 @@
-import { concat } from "../concat";
+// import { concat } from "../concat";
+import { difference } from "../difference";
 
 const array = [1, 2, 3, 4];
-const flattenArray = [1, [2, [3, [4]], 5], [[[6], 7]]];
+// const flattenArray = [1, [2, [3, [4]], 5], [[[6], 7]]];
 
-console.log(concat([1], 2, [3], [[4]]));
+// console.log(concat([1], 2, [3], [[4]]));
+// console.log(!!"" ? 1 : 0);
+// console.log(NaN !== NaN);
+const arg = { a: 1, b: 8 };
+// console.log(Object.keys(arg));
+
+console.log(difference(null, [1, 2, 3, 4, 5]));
