f12f89cd9c212c089844d945326854502b96915b
RipperZero <1018744707@qq.com>
2023-03-27 10:26:20 +0900

    feat: 3-27

--- src/app/test-factory/no-render/test.js
+++ src/app/test-factory/no-render/test.js
@@ -24,3 +24,118 @@
 //   console.log(param || 1);
 // };
 // console.log(testDefault(null));
+
+// const pA = () => {
+//   return new Promise((resolve, reject) => {
+//     resolve("Promise A resolve");
+//     // reject("Promise A reject");
+//   });
+// };
+
+// const pB = () => {
+//   return new Promise((resolve, reject) => {
+//     resolve("Promise B resolve");
+//     // reject("Promise B reject");
+//   });
+// };
+
+// const pC = () => {
+//   return new Promise((resolve, reject) => {
+//     // resolve("Promise C resolve");
+//     reject("Promise C reject");
+//   });
+// };
+// const asyncFun = async () => {
+//   const resA = await pA();
+//   console.log("AAAAAA");
+//   console.log(resA);
+//   const resB = await pB();
+//   console.log("BBBBBB");
+//   console.log(resB);
+//   console.log("CCCCCC");
+//   const resC = await pC();
+//   console.log(resC);
+// };
+// console.time("asyncFun");
+// asyncFun().catch((error) => {
+//   console.log("error error error error");
+//   console.log(error);
+// });
+// console.timeEnd("asyncFun");
+
+// const array = [1, 2, 3, 4, 5];
+
+// const arrayA = array.map((value) => {
+//   return value * 2;
+// });
+
+// console.log(array);
+// console.log(arrayA);
+
+// let resObj = {
+//   data: {
+//     expenseList: [1, 2, 3, 4, 5],
+//   },
+// };
+
+// const resObjA = resObj.data.expenseList.map((value) => {
+//   return value * 2;
+// });
+
+// console.log(resObj);
+// console.log(resObjA);
+
+const person = [
+  { id: 0, name: "小明" },
+  { id: 1, name: "小张" },
+  { id: 2, name: "小李" },
+  { id: 3, name: "小孙" },
+  { id: 1, name: "小周" },
+  { id: 2, name: "小陈" },
+];
+
+const obj = {};
+// const array = [];
+
+const reducedPerson = person.reduce((previousValue, currentValue) => {
+  // eslint-disable-next-line no-unused-expressions
+  obj[currentValue.id]
+    ? ""
+    : // : (obj[currentValue.id] = "666" && previousValue.push(currentValue));
+      (obj[currentValue.id] = 12 && previousValue.push(currentValue));
+
+  //   if (obj[currentValue.id] === undefined) {
+  //     obj[currentValue.id] = true;
+  //     previousValue.push(currentValue);
+  //     // obj[currentValue.id] = true && previousValue.push(currentValue);
+  //   }
+
+  return previousValue;
+}, []);
+
+console.log(obj);
+console.log(reducedPerson);
+// person.forEach((item) => {
+//   obj[item.id] = true && array.push(item);
+//   //   obj[item.id] = "666";
+//   //   array.push(item);
+// });
+
+// console.log(obj);
+// console.log(array);
+
+class ZeroA {
+  static aFunc = () => {
+    return "aaa";
+  };
+}
+
+ZeroA.aFunc();
+
+class ZeroB {
+  bFunc = () => {
+    return "bbb";
+  };
+}
+const objB = new ZeroB().bFunc;
+objB();
--- src/app/test-factory/no-render/test.ts
+++ src/app/test-factory/no-render/test.ts
@@ -1,25 +1,54 @@
 export {};
 
-const responseData: any = {
-  a: 1,
-  b: 2,
-};
+// interface A {
+//   a: number;
+//   aa: string;
+//   aaa: boolean;
+// }
 
-const resData: {
-  a: number;
-  b: number;
-  c?: number;
-  d?: { dd?: number };
-  e?: number | null;
-} = responseData;
+// interface B {
+//   b: undefined;
+// }
 
-console.log(resData);
-console.log("===========");
-console.log(resData.c);
-console.log("===========");
-console.log(resData.d?.dd);
-console.log("===========");
-console.log(resData.e);
-responseData["e"] = null;
-console.log("===========");
-console.log(resData.e);
+// interface C extends A, B {
+//   c: null;
+// }
+
+// type A = {
+//   a: number;
+//   aa: string;
+//   aaa: boolean;
+// };
+
+// type B = {
+//   b: undefined;
+// };
+
+// type C = A & B & { c: null };
+
+// const c: C = {
+//   a: 0,
+//   aa: "aa",
+//   aaa: true,
+//   b: undefined,
+//   c: null,
+// };
+
+// console.log(c);
+
+class ZeroA {
+  private static aFunc = () => {
+    return "aaa";
+  };
+}
+ZeroA.aFunc();
+// @ts-ignore:next-line
+ZeroA.aFunc();
+
+class ZeroB {
+  public bFunc = () => {
+    return "bbb";
+  };
+}
+const objB = new ZeroB().bFunc;
+objB();
